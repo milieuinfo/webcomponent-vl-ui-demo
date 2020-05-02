ea9a19d955d2af763981a0dcecb47391ff4cefb9
Tom Coemans <tom.coemans@gmail.com>
2020-05-02 13:25:04 +0200

    added postrelease script

--- dist/vl-demo-page.js
+++ dist/vl-demo-page.js
@@ -40,7 +40,7 @@ export class VlDemoPage extends VlElement(HTMLElement) {
                 <vl-header id="header" slot="header" data-vl-identifier="59188ff6-662b-45b9-b23a-964ad48c2bfb"></vl-header>
                 <div slot="main">
                     <vl-content-header>
-                        <img is="vl-image" slot="image" src="/src/default.jpg" srcset="/src/mobile.jpg 320w, /src/default.jpg 1024w, /src/wide.jpg 1600w">
+                        <img is="vl-image" slot="image" src="/node_modules/vl-ui-demo/dist/default.jpg" srcset="/src/mobile.jpg 320w, /src/default.jpg 1024w, /src/wide.jpg 1600w">
                         <a slot="context-link" href="https://webcomponenten.omgeving.vlaanderen.be">Vlaanderen</a>
                         <a slot="title-link" href="https://webcomponenten.omgeving.vlaanderen.be">Webcomponenten</a>
                     </vl-content-header>
