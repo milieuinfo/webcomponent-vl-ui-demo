54afefac7e05cdda6ae38633efc6de12eb4bcbc7
Tom Coemans <tom.coemans@gmail.com>
2020-05-27 18:17:28 +0200

    removed dist

--- dist/index.js
+++
@@ -1,4 +0,0 @@
-module.exports = { 
-    VlDemoPage: require('../dist/vl-demo-page.src.js').VlDemoPage,
-    VlDemo: require('../dist/vl-demo.src.js').VlDemo
-}
--- dist/vl-demo-all.src.js
+++
@@ -1,8 +0,0 @@
-import { VlDemoPage } from 'vl-ui-demo/dist/vl-demo-page.js';
-import { VlDemo } from 'vl-ui-demo/dist/vl-demo.js';
-
-export {
-    VlDemoPage,
-    VlDemo
-};
-
