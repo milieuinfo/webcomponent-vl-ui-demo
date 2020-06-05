bbc7f905ce7a30b41f2cbdd5ce36b7b56ddd03ec
coemans <tom.coemans@gmail.com>
2020-06-05 16:20:03 +0200

    added style (#9)

--- dist/vl-demo-all.src.js
+++ dist/vl-demo-all.src.js
@@ -1,8 +1,8 @@
-import { VlDemoPage } from 'vl-ui-demo/dist/vl-demo-page.src.js';
-import { VlDemo } from 'vl-ui-demo/dist/vl-demo.src.js';
+import {VlDemoPage} from vl-ui-demo/dist/vl-demo-page.src.js;
+import {VlDemo} from vl-ui-demo/dist/vl-demo.src.js;
 
 export {
-    VlDemoPage,
-    VlDemo
+  VlDemoPage,
+  VlDemo,
 };
 
--- dist/vl-demo.js
+++ dist/vl-demo.js
@@ -1,4 +1,4 @@
-import { VlElement, define } from '/node_modules/vl-ui-core/dist/vl-core.js';
+import {vlElement, define} from '/node_modules/vl-ui-core/dist/vl-core.js';
 import '/node_modules/prismjs/prism.js';
 import '/node_modules/prismjs/plugins/line-numbers/prism-line-numbers.js';
 import '/node_modules/prismjs/plugins/normalize-whitespace/prism-normalize-whitespace.js';
@@ -7,94 +7,99 @@ import '/node_modules/vl-ui-titles/dist/vl-titles.js';
 /**
  * VlDemo
  * @class
- * @classdesc 
- * 
- * @extends VlElement
- * 
+ * @classdesc
+ *
+ * @extends HTMLElement
+ * @mixin vlElement
+ *
  * @property {boolean} data-vl-title - Attribuut wordt gebruikt om de demo titel aan te geven.
- * 
+ *
  * @see {@link https://www.github.com/milieuinfo/webcomponent-vl-demo/releases/latest|Release notes}
  * @see {@link https://www.github.com/milieuinfo/webcomponent-vl-demo/issues|Issues}
  * @see {@link https://webcomponenten.omgeving.vlaanderen.be/demo/vl-demo.html|Demo}
- * 
+ *
  */
-export class VlDemo extends VlElement(HTMLElement) {
-    static get _observedAttributes() {
-        return ['title'];
-    }
+export class VlDemo extends vlElement(HTMLElement) {
+  static get _observedAttributes() {
+    return ['title'];
+  }
 
-    constructor() {
-        super(`
-            <style>
-                @import '/node_modules/vl-ui-titles/src/style.css';
-                @import '/node_modules/vl-ui-demo/dist/style.css';
-            </style>
-            <div>
-                <h3 is="vl-h3">Demo</h3>
-                <div class="demo">
-                    <slot></slot>
-                </div>
-                <pre class="line-numbers language-markup">
-                    <code></code>
-                </pre>
-            </div>
-        `);
-    }
+  constructor() {
+    super(`
+      <style>
+          @import '/node_modules/vl-ui-titles/src/style.css';
+          @import /node_modules/vl-ui-demo/dist/style.css;
+          :host {
+            display: block;
+            position: relative;
+          }
+      </style>
+      <div>
+          <h3 is="vl-h3">Demo</h3>
+          <div class="demo">
+              <slot></slot>
+          </div>
+          <pre class="line-numbers language-markup">
+              <code></code>
+          </pre>
+      </div>
+    `);
+  }
 
-    get _titleElement() {
-        return this._shadow.querySelector('h3');
-    }
+  get _titleElement() {
+    return this._shadow.querySelector('h3');
+  }
 
-    get _preElement() {
-        return this._shadow.querySelector('pre');
-    }
+  get _preElement() {
+    return this._shadow.querySelector('pre');
+  }
 
-    get _codeElement() {
-        return this._preElement.querySelector('code');
-    }
+  get _codeElement() {
+    return this._preElement.querySelector('code');
+  }
 
-    get _slotElement() {
-        return this._shadow.querySelector('slot');
-    }
+  get _slotElement() {
+    return this._shadow.querySelector('slot');
+  }
 
-    connectedCallback() {
-        this._renderCode();
-    }
+  connectedCallback() {
+    this._renderCode();
+  }
 
-    _renderCode() {
-        this._prepareCode();
-        Prism.highlightAllUnder(this._preElement);
-    }
+  _renderCode() {
+    this._prepareCode();
+    Prism.highlightAllUnder(this._preElement);
+  }
 
-    _prepareCode() {
-        const assignedElements = this._slotElement.assignedElements();
-        if (assignedElements && assignedElements.length > 0) {
-            const code = assignedElements[0].parentElement.cloneNode(true);
-            [...code.children].forEach(child => child.removeAttribute('class'));
-            this._codeElement.insertAdjacentHTML('afterbegin', this._getCode(code));
-            code.remove();
-        }
+  _prepareCode() {
+    const assignedElements = this._slotElement.assignedElements();
+    if (assignedElements && assignedElements.length > 0) {
+      const code = assignedElements[0].parentElement.cloneNode(true);
+      [...code.children].forEach((child) => child.removeAttribute('class'));
+      this._codeElement.insertAdjacentHTML('afterbegin', this._getCode(code));
+      code.remove();
     }
+  }
 
-    _titleChangedCallback(oldValue, newValue) {
-        this._titleElement.innerText = newValue;
-    }
+  _titleChangedCallback(oldValue, newValue) {
+    this._titleElement.innerText = newValue;
+  }
 
-    _getCode(element) {
-        let code = element.innerHTML;
-        code = this._escapeTags(code);
-        code = this._stripEmptyAttributeDefinition(code);
-        return code;
-    }
+  _getCode(element) {
+    let code = element.innerHTML;
+    code = this._escapeTags(code);
+    code = this._stripEmptyAttributeDefinition(code);
+    return code;
+  }
 
-    _escapeTags(code) {
-        const regex = /[\u00A0-\u9999<>&](?!#)/gim;
-        return code.replace(regex, match => '&#' + match.charCodeAt(0) + ';');
-    }
+  _escapeTags(code) {
+    const regex = /[\u00A0-\u9999<>&](?!#)/gim;
+    return code.replace(regex, (match) => '&#' + match.charCodeAt(0) + ';');
+  }
 
-    _stripEmptyAttributeDefinition(code) {
-        return code.split('=""').join('');
-    }
+  _stripEmptyAttributeDefinition(code) {
+    return code.split('=""').join('');
+  }
 }
 
 define('vl-demo', VlDemo);
