d846246a8e79bdb7a8ef2b565ab3c2bf27152c92
coemans <tom.coemans@gmail.com>
2020-08-13 11:34:49 +0200

    Introductie vl-ui-code-preview (#21)

--- dist/vl-demo-all.js
+++ dist/vl-demo-all.js
@@ -1,7 +1,7 @@
-import {VlDemoPage} from '/node_modules/vl-ui-demo/dist/vl-demo-page.js';
 import {VlDemo} from '/node_modules/vl-ui-demo/dist/vl-demo.js';
+import {VlDemoPage} from '/node_modules/vl-ui-demo/dist/vl-demo-page.js';
 
 export {
-  VlDemoPage,
   VlDemo,
+  VlDemoPage,
 };
