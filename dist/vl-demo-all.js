b80529f639c25c10b315edf1262d344df538cec5
Tom Coemans <tom.coemans@gmail.com>
2020-08-13 12:59:13 +0200

    dist cache

--- dist/vl-demo-all.js
+++
@@ -1,7 +0,0 @@
-import {VlDemo} from '/node_modules/vl-ui-demo/dist/vl-demo.js';
-import {VlDemoPage} from '/node_modules/vl-ui-demo/dist/vl-demo-page.js';
-
-export {
-  VlDemo,
-  VlDemoPage,
-};
--- dist/vl-demo-all.src.js
+++
@@ -1,8 +0,0 @@
-import {VlDemo} from 'vl-ui-demo/dist/vl-demo.src.js';
-import {VlDemoPage} from 'vl-ui-demo/dist/vl-demo-page.src.js';
-
-export {
-  VlDemo,
-  VlDemoPage,
-};
-
